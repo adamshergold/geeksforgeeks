882b4e82b5ac757a448a8f0ce88c7c0a295d6031
Adam Shergold <mail@adamshergold.co.uk>
2018-03-16 18:41:51 +0000

    more algos

--- lib.test/test.arrays.cs
+++ lib.test/test.arrays.cs
@@ -30,7 +30,71 @@ namespace GeeksForGeeks.Test
                 yield return new TestCaseData("1 2 3 4 5 6 7 8 9 10", 15).Returns("1 5");
                 yield return new TestCaseData("1 2 3 7 5", 12).Returns("2 4");
             }
-            
+
+            public static IEnumerable<TestCaseData> SortArrayOfZerosOnesTwos()
+            {
+                yield return new TestCaseData("0 2 1 2 0").Returns("0 0 1 2 2");
+            }
+
+            public static IEnumerable<TestCaseData> Equilibrium()
+            {
+                yield return new TestCaseData("1 1").Returns(-1);
+                yield return new TestCaseData("1 2 3").Returns(-1);
+                yield return new TestCaseData("1 2 1").Returns(2);
+                yield return new TestCaseData("1").Returns(1);
+                yield return new TestCaseData("1 3 5 2 2").Returns(3);
+            }
+
+            public static IEnumerable<TestCaseData> MaxSumIncreasingSubsequence()
+            {
+                yield return new TestCaseData("1 90 90 2 3 100 4 5").Returns(281);
+                yield return new TestCaseData("1 101 2 3 100 4 5").Returns(106);
+                yield return new TestCaseData("10 5 4 3").Returns(10);
+            }
+
+            public static IEnumerable<TestCaseData> Leaders()
+            {
+                yield return new TestCaseData("16 17 4 3 5 2").Returns("17 5 2");
+                yield return new TestCaseData("1 2 3 4 0").Returns("4 0");
+            }
+        }
+
+        [TestFixture]
+        public class LeadersTests
+        {
+            [TestCaseSource(typeof(TestCases), "Leaders")]
+            public string Test(string sv)
+            {
+                return GeeksForGeeks.Arrays.Leaders(Helpers.StringToArray(sv));
+            }
+        }
+        [TestFixture]
+        public class MaxSumIncreasingSubsequenceTests
+        {
+            [TestCaseSource(typeof(TestCases), "MaxSumIncreasingSubsequence")]
+            public int Test(string sv)
+            {
+                return GeeksForGeeks.Arrays.MaxSumIncreasingSubsequence(Helpers.StringToArray(sv));
+            }
+        }
+        [TestFixture]
+        public class EquilibriumTests
+        {
+            [TestCaseSource(typeof(TestCases),"Equilibrium")]
+            public int Test(string sv)
+            {
+                return GeeksForGeeks.Arrays.Equilibrium(Helpers.StringToArray(sv));
+            }
+        }
+        
+        [TestFixture]
+        public class SortArrayOfZerosOnesTwos
+        {
+            [TestCaseSource(typeof(TestCases), "SortArrayOfZerosOnesTwos")]
+            public string Test(string sv)
+            {
+                return GeeksForGeeks.Arrays.SortArrayOfZerosOnesTwos(Helpers.StringToArray(sv));
+            }
         }
 
         [TestFixture]
--- lib/arrays.cs
+++ lib/arrays.cs
@@ -1,11 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 
 namespace GeeksForGeeks
 {
     public class Arrays
     {
+        public static string Leaders(int[] vs)
+        {
+            var leaders = new Stack<int>();
+            
+            var max = vs[vs.Length - 1];
+
+            leaders.Push(max);
+
+            for (var i = vs.Length - 2; i >= 0; i--)
+            {
+                if (vs[i] > max)
+                {
+                    leaders.Push(vs[i]);
+                    max = vs[i];
+                }
+            }
+
+            var sb = new StringBuilder();
+            while (leaders.Count > 0)
+            {
+                sb.AppendFormat("{0} ", leaders.Pop());
+            }
+
+            return sb.ToString().Trim();
+        }
+        
+        public static int MaxSumIncreasingSubsequence(int[] vs)
+        {
+            if (vs.Length == 0)
+            {
+                throw new System.Exception("Invalid length!");
+            }
+
+            var ms = new int[vs.Length];
+
+            for (var i = 0; i < ms.Length; i++)
+            {
+                ms[i] = 0;
+            }
+
+            ms[0] = vs[0];
+            
+            for (var i = 1; i < ms.Length; i++)
+            {
+                for (var j = 0; j < i; j++)
+                {
+                    ms[i] = vs[i] >= vs[j] && ms[j] + vs[i] > ms[i] ? ms[j] + vs[i] : ms[i];
+                }
+            }
+
+            return ms.Max();
+
+        }
+
+        public static int Equilibrium(int[] vs)
+        {
+            if( vs.Length == 0 || vs.Length == 2 )
+            {
+                return -1;
+                //throw new System.Exception("Invalid length!");
+            }
+            
+            if( vs.Length == 1 )
+            {
+                return vs[0];
+            }
+
+            var lsum = vs[0];
+            var rsum = vs.Skip(2).Sum();
+            var done = lsum == rsum;
+            var idx = 1; 
+            
+            for( ; idx < vs.Length-1 && !done; idx++ )
+            {
+                lsum += vs[idx];
+                rsum -= vs[idx+1];
+
+                done = lsum == rsum;
+            }
+
+            return lsum == rsum ? idx + 1 : -1;
+        }
+        
+        public static string SortArrayOfZerosOnesTwos(int[] vs)
+        {
+            var zero = 0;
+            var one = 0;
+            var two = 0;
+
+            foreach (var v in vs)
+            {
+                switch (v)
+                {
+                    case 0:
+                        zero++;
+                        break;
+                    case 1:
+                        one++;
+                        break;
+                    case 2:
+                        two++;
+                        break;
+                }
+            }
+
+            var sb = new StringBuilder();
+
+            sb.Insert(0, "0 ", zero);
+            sb.Insert(sb.Length, "1 ", one);
+            sb.Insert(sb.Length, "2 ", two);
+
+            return sb.ToString().Trim();
+        }
+
+
         public static string SubArrayWithGivenSum(int[] vs, int expected)
         {
             var i = 0;
